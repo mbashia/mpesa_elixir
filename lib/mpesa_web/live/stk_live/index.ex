a27ba94c4e0dda5cacf071aaad5de034de810faa
mbashia <mbashiavictor@gmail.com>
2024-12-03 05:11:11 +0300

    Implement page to test stk transaction

--- lib/mpesa_web/live/stk_live/index.ex
+++ lib/mpesa_web/live/stk_live/index.ex
@@ -9,12 +9,13 @@ defmodule MpesaWeb.Stklive.Index do
   """
   use MpesaWeb, :live_view
 
+  alias Mpesa.MpesaStk
   alias Mpesa.StkForm
+  alias Mpesa.TransactionStatusChecker
 
   require Logger
 
   def mount(_params, _session, socket) do
-    # Create an empty changeset for initializing the form
     initial_changeset = StkForm.changeset(%{}, %{})
 
     {:ok,
@@ -35,6 +36,62 @@ defmodule MpesaWeb.Stklive.Index do
 
     {:noreply, assign(socket, form: to_form(changeset, action: :validate, as: "stk_form"))}
   end
+
+  def handle_event(
+        "pay",
+        %{"stk_form" => %{"Phone_number" => phone_number, "amount" => amount}},
+        socket
+      ) do
+    payment_initiation = MpesaStk.initiate_payment(phone_number, amount)
+
+    case payment_initiation do
+      {:ok, response_body} ->
+        checkout_request_id = response_body["CheckoutRequestID"]
+
+        case recursive_check_status(checkout_request_id, 200) do
+          {:ok, response} ->
+            {:noreply,
+             socket
+             |> put_flash(:info, response)
+             |> push_navigate(to: "/stk-test")}
+
+          {:error, message} ->
+            {:noreply,
+             socket
+             |> put_flash(:error, message)
+             |> push_navigate(to: "/stk-test")}
+        end
+
+      {:error, _} ->
+        {:noreply,
+         socket
+         |> put_flash(:error, "Failed to initiate payment")
+         |> push_navigate(to: "/stk-test")}
+    end
+  end
+
+  defp recursive_check_status(checkout_request_id, retries_remaining)
+       when retries_remaining > 0 do
+    case TransactionStatusChecker.check_status(checkout_request_id) do
+      {:ok, "The transaction is being processed"} ->
+        # Pause for 2 seconds before retrying
+        Process.sleep(2000)
+        Logger.info("Retrying transaction status check: #{retries_remaining} attempts remaining")
+        recursive_check_status(checkout_request_id, retries_remaining - 1)
+
+      {:ok, response} ->
+        {:ok, response}
+
+      {:error, reason} ->
+        {:error, reason}
+
+      _ ->
+        {:error, "Transaction failed"}
+    end
+  end
+
+  defp recursive_check_status(_checkout_request_id, 0),
+    do: {:error, "Failed to get a valid status after multiple attempts"}
 end
 
 ## test- if phone number is valid
